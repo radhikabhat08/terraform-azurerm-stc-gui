9062b4770729954f810367a962bf668a8da6dd32
radhikabhat08 <Radhika.Bhat@spirent.com>
2021-01-27 15:23:42 +0530

    fixed readme

--- examples/run-stc-gui/variable.tf
+++ examples/run-stc-gui/variable.tf
@@ -1,5 +1,4 @@
 variable "stc_windows_pw" {
   description = "Specify the windows password with a TF_VAR_stc_windows_pw environment variable"
   type        = string
-  sensitive   = true
 }
--- variables.tf
+++ variables.tf
@@ -78,7 +78,6 @@ variable "ingress_cidr_blocks" {
 variable "stc_windows_pw" {
   description = "Specify the windows password with a TF_VAR_stc_windows_pw environment variable"
   type        = string
-  sensitive   = true
 }
 
 
